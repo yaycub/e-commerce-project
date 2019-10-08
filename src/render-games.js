1d03bdd7083ffa353387dbfc929f84694c4af1e6
Jacob Harrington <jharrington4@gmail.com>
2019-10-07 17:11:43 -0700

    finished render-games.js to pass all tests

--- src/render-games.js
+++ src/render-games.js
@@ -1,3 +1,32 @@
 export default (gameData) => {
-    
-}
+    const li = document.createElement('li');
+    li.id = 'game-list';
+
+    const h2 = document.createElement('h2');
+    h2.textContent = gameData.name;
+    h2.id = 'game-name';
+    li.appendChild(h2);
+
+    const imageDiv = document.createElement('div');
+    const img = document.createElement('img');
+    img.src = gameData.image;
+    imageDiv.appendChild(img);
+    li.appendChild(imageDiv);
+
+    const descriptionDiv = document.createElement('div');
+    descriptionDiv.id = 'game-description';
+    descriptionDiv.textContent = gameData.description;
+    li.appendChild(descriptionDiv);
+
+    const categoryDiv = document.createElement('div');
+    categoryDiv.id = 'game-category';
+    categoryDiv.textContent = gameData.category;
+    li.appendChild(categoryDiv);
+
+    const priceDiv = document.createElement('div');
+    priceDiv.id = 'game-price';
+    priceDiv.textContent = '$' + gameData.price;
+    li.appendChild(priceDiv);
+
+    return li;
+};
--- test/render-games-test.js
+++ test/render-games-test.js
@@ -1,16 +1,29 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
+import renderGames from '../src/render-games.js';
 
 const test = QUnit.test;
 
 test('time to test a function', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const skyrim = {
+        id: 'skyrim',
+        name: 'Skyrim',
+        image: '/assets/skyrim.png',
+        description: 'An open-world Action RPG',
+        category: 'RPG',
+        price: 40
+    };
+    const expected = '<li id="game-list"><h2 id="game-name">Skyrim</h2><div><img src="/assets/skyrim.png"></div><div id="game-description">An open-world Action RPG</div><div id="game-category">RPG</div><div id="game-price">$40</div></li>';
 
     //Act 
     // Call the function you're testing and set the result to a const
 
+    const dom = renderGames(skyrim);
+    const html = dom.outerHTML;
+
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    assert.equal(html, expected);
 });
